ddc9c01cf1368cb873921aa1d6cc066e4d8b2f2b
Ziggear <ziggear@gmail.com>
2015-05-06 13:13:28 +0800

    Initial Commit

---
+++ JSBridgeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSBridgeTest
+//
+//  Created by ziggear on 15-5-6.
+//  Copyright (c) 2015年 ziggear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JSBridgeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSBridgeTest
+//
+//  Created by ziggear on 15-5-6.
+//  Copyright (c) 2015年 ziggear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
